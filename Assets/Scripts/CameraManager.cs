af960d0942879a7757a75a962b883a9169b6975e
Crazytrain1 <maximeplourde0@gmail.com>
2022-06-11 18:39:41 -0400

    fix player not moving bug+ small integration

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -7,6 +7,9 @@ public class CameraManager : MonoBehaviour
     public GameObject  Camera1;
     public GameObject Camera2;
     public GameObject Camera3;
+    public GameObject Camera4;
+    public GameObject Camera5;
+    
     public GameObject Player;
     public Transform PlayerPosition;
     
@@ -16,6 +19,10 @@ public class CameraManager : MonoBehaviour
         Camera1.SetActive(true);
         Camera2.SetActive(false);
         Camera3.SetActive(false);
+        Camera4.SetActive(false);
+        Camera5.SetActive(false);
+        
+
     }
 
     // Update is called once per frame
