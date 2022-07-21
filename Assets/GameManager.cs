4b59a4a2f493fe2f33bf94be4a284c2dde0e725a
Crazytrain1 <maximeplourde0@gmail.com>
2022-07-21 13:43:52 -0400

    Transition between Level

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using Cinemachine;
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject Spawner;
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject LookAt;
+    [SerializeField] GameObject parent;
+    [SerializeField] Transform parentT;
+    [SerializeField] enum Level {MainMenu,Level0,Level1,Level2,Level3, Level4};
+    [SerializeField] Level CurrentLevel;
+    
+
+    void Start()
+
+    {
+        
+        parent = Instantiate(Player as GameObject);
+        
+        Player.transform.position = Spawner.transform.position;
+        LookAt.transform.position = parent.transform.position;
+        //
+        parentT = parent.transform;
+        LookAt.transform.SetParent(parentT, true);
+        
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="Player")
+        {
+            SceneManager.LoadScene((int)CurrentLevel);
+        }
+
+    }
+}
