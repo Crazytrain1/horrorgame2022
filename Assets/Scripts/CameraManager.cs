3e6b88fe9d2979e1331d4b8af3fc07045793f343
Crazytrain1 <maximeplourde0@gmail.com>
2022-07-22 21:03:43 -0400

    Implémentation caméra niveau 1

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [Header("Camera Level 0")]
     public GameObject  CameraBureau;
     public GameObject CameraHallway;
     public GameObject CameraWaitingRoom;
@@ -16,6 +17,25 @@ public class CameraManager : MonoBehaviour
     public GameObject CameraStaircase1;
     public GameObject CameraStaircase2;
 
+    [Header("Camera Level 1")]
+    public GameObject CameraStair;
+    public GameObject CameraLongHallway;
+    public GameObject CameraEmptyRoom;
+    public GameObject CameraLongEmptyHallway;
+    public GameObject CameraBackOfLoreRoom;
+    public GameObject CameraSideOfLoreRoom;
+    public GameObject CameraLoreRoom;
+    public GameObject CameraCorridorEmpty;
+    public GameObject CameraHugeRoom;
+    public GameObject CameraOpenLoreRoom;
+    public GameObject CameraHugeOpenLoreRoom;
+    public GameObject CameraCorridorLeaving1;
+    public GameObject CameraCorridorLeaving2;
+    
+
+
+    [SerializeField] enum Level { MainMenu, Level0, Level1, Level2, Level3, Level4 };
+    [SerializeField] Level NextLevel;
 
     public GameObject Player;
     public Transform PlayerPosition;
@@ -23,17 +43,36 @@ public class CameraManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CameraBureau.SetActive(true);
-        CameraHallway.SetActive(false);
-        CameraWaitingRoom.SetActive(false);
-        CameraBathroom.SetActive(false);
-        CameraWorkstation.SetActive(false);
-        CameraHallwayTransition1.SetActive(false);
-        CameraHallwayTransition2.SetActive(false);
-        CameraHallwayTransition3.SetActive(false);
-        CameraHallwayTransition4.SetActive(false);
-        CameraStaircase1.SetActive(false);
-        CameraStaircase2.SetActive(false);
+        if (NextLevel == Level.Level0)
+        {
+            CameraBureau.SetActive(true);
+            CameraHallway.SetActive(false);
+            CameraWaitingRoom.SetActive(false);
+            CameraBathroom.SetActive(false);
+            CameraWorkstation.SetActive(false);
+            CameraHallwayTransition1.SetActive(false);
+            CameraHallwayTransition2.SetActive(false);
+            CameraHallwayTransition3.SetActive(false);
+            CameraHallwayTransition4.SetActive(false);
+            CameraStaircase1.SetActive(false);
+            CameraStaircase2.SetActive(false);
+        }
+        else if (NextLevel == Level.Level1)
+        {
+            CameraStair.SetActive(true);
+            CameraLongHallway.SetActive(false);
+            CameraEmptyRoom.SetActive(false);
+            CameraLongEmptyHallway.SetActive(false);
+            CameraBackOfLoreRoom.SetActive(false);
+            CameraSideOfLoreRoom.SetActive(false);
+            CameraLoreRoom.SetActive(false);
+            CameraCorridorEmpty.SetActive(false);
+            CameraHugeRoom.SetActive(false);
+            CameraOpenLoreRoom.SetActive(false);
+            CameraHugeOpenLoreRoom.SetActive(false);
+            CameraCorridorLeaving1.SetActive(false);
+            CameraCorridorLeaving2.SetActive(false);
+        }
     }
 
     // Update is called once per frame
