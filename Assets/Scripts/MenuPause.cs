3e665786a94f9dcda153df953732e5235a355d84
MaximePlourde <98863560+Crazytrain1@users.noreply.github.com>
2022-12-28 20:14:07 -0500

    remake main menu

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+
+    public void OnReume()
+    {
+        GameManager.Instance.UpdateGameState(GameManager.Instance.PreviousState);
+    }
+    public void OnMainMenu()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+
+}
