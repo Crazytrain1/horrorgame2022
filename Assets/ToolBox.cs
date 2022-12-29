dc33856de96c41469577babca75f9a432ce5b71b
MaximePlourde <98863560+Crazytrain1@users.noreply.github.com>
2022-12-29 00:19:11 -0500

    commit

---
+++ Assets/ToolBox.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBox : MonoBehaviour
+{
+    [SerializeField] GameObject qteSlider;
+    [SerializeField] float TheDistance;
+    [SerializeField] GameObject InteractDisplayObject;
+    public bool CanInteract = true;
+    private InteractDisplay _InteractDisplay;
+    private bool _open;
+    private int _DistanceMax = 2;
+    private bool _CanClose = false;
+
+    public void Start()
+    {
+        _InteractDisplay = InteractDisplayObject.GetComponent<InteractDisplay>();
+
+        _InteractDisplay.UpdateInteractDisplay();
+
+    }
+    private void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+
+        if (Input.GetKeyDown("e") && GameManager.Instance.State == GameManager.GameState.interacting && _open && _CanClose)
+        {
+
+
+            GameManager.Instance.UpdateGameState(GameManager.GameState.Playing);
+            qteSlider.SetActive(false);
+            _open = false;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (CanInteract)
+        {
+            if (TheDistance <= _DistanceMax)
+            {
+
+                _InteractDisplay.SetInteractDisplay(true, null, "search toolbox");
+
+
+            }
+            else
+            {
+                _InteractDisplay.UpdateInteractDisplay();
+
+            }
+            if (Input.GetKeyDown("e") && TheDistance <= _DistanceMax && !_open)
+            {
+                GameManager.Instance.UpdateGameState(GameManager.GameState.interacting);
+
+                qteSlider.SetActive(true);
+                _open = true;
+                _CanClose = false;
+                StartCoroutine("Delay");
+
+            }
+
+
+
+
+
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForEndOfFrame();
+        _CanClose = true;
+    }
+
+    void OnMouseExit()
+    {
+        _InteractDisplay.UpdateInteractDisplay();
+    }
+}
