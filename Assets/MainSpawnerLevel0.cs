16eb7f33e08680ec785826f1857fa375f9049c8b
MaximePlourde <98863560+Crazytrain1@users.noreply.github.com>
2023-01-16 15:28:59 -0500

    fix bug spawning in the abyss?!?!?!

--- Assets/MainSpawnerLevel0.cs
+++ Assets/MainSpawnerLevel0.cs
@@ -7,11 +7,22 @@ public class MainSpawnerLevel0 : MonoBehaviour
 {
     [SerializeField] GameObject Player;
     [SerializeField] GameObject parent;
-    void Start()
+
+    private void Awake()
     {
         parent = Instantiate(Player as GameObject);
+    }
+
+
+    void Start()
+    {
+        
 
         parent.transform.position = this.transform.position;
+        //Debug.Log("position spawner");
+        //Debug.Log(transform.position);
+        //Debug.Log("position player");
+        //Debug.Log(parent.transform.position);
         GameManager.Instance.UpdateGameState(GameState.Playing);
     }
 
